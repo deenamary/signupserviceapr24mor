89bb95198716c21ac0a4d6b8e293584dbd422f6d
deena <deenaantonym@gmail.com>
2024-05-07 17:54:07 +0530

    Initial commit

---
+++ src/main/java/com/example/signupserviceapr24mor/Signupserviceapr24morApplication.java
@@ -0,0 +1,15 @@
+package com.example.signupserviceapr24mor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Signupserviceapr24morApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Signupserviceapr24morApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/signupserviceapr24mor/Signupserviceapr24morApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signupserviceapr24mor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Signupserviceapr24morApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
